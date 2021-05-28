ffb397e1e878282b5128ca69697b246e1fbc2e32
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-05-28 02:59:06 -0700

    Update misc.py

--- LaylaRobot/modules/helper_funcs/misc.py
+++ LaylaRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="yone_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
 
     return pairs
 
