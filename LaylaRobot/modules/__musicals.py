cde1bb61667eb09a2b2579490a3ec3ebc71b5414
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-05-28 08:22:17 -0700

    Update __musicals.py

--- LaylaRobot/modules/__musicals.py
+++ LaylaRobot/modules/__musicals.py
@@ -2,7 +2,7 @@ __mod_name__ = "◎Music"
 
 __help__ = """
 • `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
-• `/video`** <songname artist(optional)>: uploads the video song in it's best quality available.
+• `/video`** <songname artist(optional)>: download the video song in it's best quality available.
 • `/deezer`** <songname>: download from deezer
 • `/lyrics`** <songname artist(optional)>: sends the complete lyrics of the song provided as input
 • `/glyrics`** <i> song name </i> : This plugin searches for song lyrics with song name and artist.
