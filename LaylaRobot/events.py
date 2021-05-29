78e6f44c28531f1764a36c54ea280a1f51c1dbf7
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-05-28 20:25:10 -0700

    Update events.py

--- LaylaRobot/events.py
+++ LaylaRobot/events.py
@@ -62,3 +62,42 @@ def callbackquery(**args):
         return func
 
     return decorator
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import MashaRoBot.events
+
+        path = Path(f"LaylaRoBot/modules/{shortname}.py")
+        name = "LaylaRoBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Successfully imported " + shortname)
+    else:
+        import importlib
+        import MashaRoBot.events
+
+        path = Path(f"LaylaRoBot/modules/{shortname}.py")
+        name = "LaylaRoBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.register = register
+        mod.laylarobot = laylarobot
+        mod.tbot = telethn
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        sys.modules["LaylaRoBot.modules." + shortname] = mod
+        print("Successfully imported " + shortname)
+
+
+path = "LaylaRoBot/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
