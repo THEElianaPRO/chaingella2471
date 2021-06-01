760da3bfbe5e192b05abf5b87d52b04f73310b34
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-01 07:28:27 -0700

    Update __aifunctions.py

--- LaylaRobot/modules/__aifunctions.py
+++ LaylaRobot/modules/__aifunctions.py
@@ -1,6 +1,6 @@
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from LaylaRoBot.events import register
+from LaylaRobot.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
