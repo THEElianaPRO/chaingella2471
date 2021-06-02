841a0e12bdeceee3b6d756a113f56b6459f687dd
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-02 04:24:52 -0700

    Update zombies.py

--- LaylaRobot/modules/zombies.py
+++ LaylaRobot/modules/zombies.py
@@ -1,5 +1,3 @@
- # zombies Module Developed and Provided by @uday_gondaliya 
-
 import asyncio
 from asyncio import sleep
 
@@ -113,3 +111,45 @@ async def zombies(event):
         \n`{del_a}` Zombie Admin Accounts Are Not Removed!"
 
     await cleaning_zombies.edit(del_status)
+    
+    
+from telethon.tl.types import UserStatusLastMonth, UserStatusLastWeek, ChatBannedRights
+from LaylaRobot.events import register
+from telethon import *
+from telethon.tl.functions.channels import (EditBannedRequest)
+                                            
+
+@register(pattern="^/kickthefools")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not event.chat.admin_rights.ban_users:
+        return
+    if not admin and not creator:
+        await event.reply("I am not admin here !")
+        return
+    c = 0
+    KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
+    await event.reply("Searching Participant Lists...")
+    async for i in event.client.iter_participants(event.chat_id):
+
+        if isinstance(i.status, UserStatusLastMonth):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1
+                    
+        if isinstance(i.status, UserStatusLastMonth):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1                    
+
+    required_string = "Successfully Kicked **{}** users"
+    await event.reply(required_string.format(c))
+
