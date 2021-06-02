e2a9ed5e60ef2e0eed5f5ab48cef54483af84801
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-02 03:16:33 -0700

    Create musicplayer.py

---
+++ LaylaRobot/modules/musicplayer.py
@@ -0,0 +1,22 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+❍ /play <song name> - play song you requested
+❍ /dplay <song name> - play song you requested via deezer
+❍ /splay <song name> - play song you requested via jio saavn
+❍ /playlist - Show now playing list
+❍ /current - Show now playing
+❍ /song <song name> - download songs you want quickly
+❍ /search <query> - search videos on youtube with details
+❍ /deezer <song name> - download songs you want quickly via deezer
+❍ /saavn <song name> - download songs you want quickly via saavn
+❍ /video <song name> - download videos you want quickly
+*Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /admincache - Refresh admin list
+"""
