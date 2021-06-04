a88799e1cbf38e961dc01d2d72df0065100c97b4
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-04 08:19:35 -0700

    Update __tools.py

--- LaylaRobot/modules/__tools.py
+++ LaylaRobot/modules/__tools.py
@@ -10,6 +10,9 @@ __help__ = """
  ❍ /weather <city>*:* Get weather info in a particular place.
  ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
  ❍ /wttr moon*:* Get the current status of moon
+ 
+*Quotly*
+- /q : reply any message with this command to create a sticker.
 
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
