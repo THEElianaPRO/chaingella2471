bc74633cc8a7ae4baa595d558152b6e74762dea4
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-04 19:29:11 +0530

    Update __nightmode.py

--- LaylaRobot/modules/__nightmode.py
+++ LaylaRobot/modules/__nightmode.py
@@ -294,3 +294,11 @@ async def _(event):
         print(e)
 
 
+__help__ = f"""
+ ❍ /nightmode on/off
+ 
+Set NightMode - /setnightmode timezone : 12:00:00 PM : 07:00:00 AM
+**Note:** Now you can choose your counrty time zone and opening and closing time also.
+"""
+
+__mod_name__ = "NightMode"
