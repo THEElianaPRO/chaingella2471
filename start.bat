30f623bae2110140527be69c135b2f8c55cfd1c1
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-06-05 02:22:20 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Yone Robot
+TITLE Layla Robot
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
+env\scripts\activate.bat && py -m LaylaRobot
